47490ac133924868dfccafc675ea78ad61f47c5e
fly-high2016 <fly-high@live.cn>
2018-01-25 16:59:14 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/pincal/ding""
    This reverts commit 1df76ebc6d35cdf473e64f4fea546c86e263697c.

---
+++ local.py
@@ -0,0 +1,17 @@
+#!/usr/bin python
+# _*_ coding:utf-8 _*_
+# author:liuxx
+# time:2018.1.24
+
+class Local(object):
+    """docstring for Local."""
+    def __init__(self, arg):
+        super(Local, self).__init__()
+        self.arg = arg
+
+    # 创建本地数据库
+    def creat():
+        
+
+    # 更新本地数据库
+    def update():
---
+++ week.py
@@ -0,0 +1,9 @@
+#!/usr/bin python
+# _*_ coding:utf-8 _*_
+# author:liuxx
+# time:2018.1.24
+
+def week_task():
+    print 'week'
+    # while(True):
+        # 取数据
