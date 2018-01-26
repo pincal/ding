96f1dc64edd7e7da059acad53f5437e664b493d7
fly-high2016 <fly-high@live.cn>
2018-01-26 11:20:04 +0800

    2018年1月26日11:19:42增加了access_token定时刷新

---
+++ ding_api/dingapi_timer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import threading
+import auth
+import config
+
+
+#程序启动时直接调用access_token_timer函数
+#在需要token的地方直接使用dingapi.access_token即可
+#每隔100分钟刷新一次，刷新失败则暂停5分钟再刷新
+def access_token_timer():
+    global at_timer
+    global access_token
+    at_timer = threading.Timer(6000, access_token_timer)
+    at_timer.start()
+    
+    is_success, result = auth.get_access_token(config.CorpID, config.Secret)
+    if is_success == True:
+        access_token = result.get('access_token')
+        expires_in = result.get('expires_in')
+        #print access_token #debug only
+    else:
+        sleep(300)
+        #print 'sleep' #debug only
