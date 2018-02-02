bb9ca9746e0f0190e44bdc311a76728d112a34ac
fly-high2016 <fly-high@live.cn>
2018-02-02 15:20:08 +0800

    2018年2月2日15:18:31读取钉钉demo
    user_one_key_store()一键读取钉钉部门详情清单和人员详情清单。并分别存放位于两个detail空表中。下一步解决如果数据库中有数据怎么处理。

--- dingread.py
+++ dingread.py
@@ -7,9 +7,10 @@ from dingding_sdk import department
 from dingding_sdk import utils
 import MySQLdb
 
-
+#获取access_token
 dingapi_timer.access_token_timer()
 
+#连接数据库
 db = MySQLdb.connect('localhost','root','yoyoball','test')
 db.set_character_set('utf8') #修改MySQLdb默认编码
 cursor= db.cursor()
@@ -18,9 +19,11 @@ cursor.execute('SET CHARACTER SET utf8;') #修改MySQLdb默认编码
 cursor.execute('SET character_set_connection=utf8;') #修改MySQLdb默认编码
 
 
+
+
 #初始化部门清单
-def read_department_list_first():
-    is_success, result = department.get_department_list(dingapi_timer.access_token)
+def store_department_list(fetch_child=True, parent_id=1):
+    is_success, result = department.get_department_list(dingapi_timer.access_token, fetch_child, parent_id)
     #print result #debug only
     if is_success == True:
         for i in range(len(result['department'])):
@@ -42,11 +45,12 @@ def read_department_list_first():
             except MySQLdb.Error, e:  
                 sqlError =  "Error:%s" % str(e)
                 print sqlError
-            
-def read_department_detail_first(departmentid):
-    is_success, result = department.get_department_detail(dingapi_timer.access_token, departmentid)
+
+          
+def store_department_detail(department_id):
+    is_success, result = department.get_department_detail(dingapi_timer.access_token, department_id)
     #print result #debug only
-    if is_success == True and departmentid != 1:  #id为1的为根部门，根部门钉钉不会返回parentid
+    if is_success == True and department_id != 1:  #id为1的为根部门，根部门钉钉不会返回parentid,在数据库中将根部门的parentid设置为0
         ding_read_sql = "INSERT INTO dingding_department_detail(id, name, \
                         parentid, orders, createDeptGroup, autoAddUser, deptHiding, \
                         deptPermits, userPermits, outerDept, outerPermitDepts, \
@@ -59,19 +63,94 @@ def read_department_detail_first(departmentid):
                          result['orgDeptOwner'], result['deptManagerUseridList'])
     else:
         ding_read_sql = "INSERT INTO dingding_department_detail(id, name, \
-                        orders, createDeptGroup, autoAddUser, deptHiding, \
+                        parentid, orders, createDeptGroup, autoAddUser, deptHiding, \
                         deptPermits, userPermits, outerDept, outerPermitDepts, \
                         outerPermitUsers, orgDeptOwner, deptManagerUseridList) \
-                        VALUES('%d', '%s', '%d', '%d', '%d', '%d', '%s', '%s', '%d', '%s', '%s', '%s', '%s')" % \
-                        (result['id'], result['name'], result['order'],
+                        VALUES('%d', '%s','%d', '%d', '%d', '%d', '%d', '%s', '%s', '%d', '%s', '%s', '%s', '%s')" % \
+                        (result['id'], result['name'],0, result['order'],
                          result['createDeptGroup'], result['autoAddUser'], result['deptHiding'],
                          result['deptPerimits'], result['userPerimits'], result['outerDept'],
                          result['outerPermitDepts'], result['outerPermitUsers'],
                          result['orgDeptOwner'], result['deptManagerUseridList'])        
 
-        print ding_read_sql #debug only
+    #print ding_read_sql #debug only
+    try:
+        cursor.execute(ding_read_sql)
+        db.commit()
+    except MySQLdb.Warning, w:  
+        sqlWarning =  "Warning:%s" % str(w)
+        print sqlWarning
+    except MySQLdb.Error, e:  
+        sqlError =  "Error:%s" % str(e)
+        print sqlError
+   
+
+def store_all_department_detail():
+    store_department_detail(1) #获取根部门详情
+    cursor.execute("SELECT id FROM dingding_department_list")
+    result = cursor.fetchall() #获取根部门以下所有部门id
+    #print type(result), len(result), result #debug only 讨厌返回了一个二级的tuple
+    if result != None and len(result) > 0:
+        for i in range(len(result)):
+            if result[i][0] != 1: #这个应该始终为真。因为department_list不应该有id=1的条目。主要是防止写重复id时出错
+                store_department_detail(result[i][0]) #获取根部门以下所有部门详情
+    
+#一键获取所有部门的所有数据
+def department_one_key_store():
+    store_department_list(fetch_child=True, parent_id=1)
+    store_all_department_detail()
+
+#根据部门获取人员基本信息，对于具有相同userid的人员记录只存储一次
+#确保user_list表没有重复的人员
+def store_user_list(department_id, offset=None, size=None, order=None):
+    is_success, result = user.get_department_simple_userlist(dingapi_timer.access_token, department_id, offset, size, order)
+    if is_success == True and result['userlist'] != None and len(result['userlist']) > 0:
+        for i in range(len(result['userlist'])):
+            check_db = "SELECT userid FROM dingding_user_list WHERE userid=%s" % result['userlist'][i]['userid']
+            cursor.execute(check_db) #确保不会出现重复主键，当然也可以去直接写，捕获错误。
+            db_result = cursor.fetchone()
+            if db_result == None: #查询为空时返回NONE 
+                write_db = "INSERT INTO dingding_user_list(userid, name) VALUES('%s', '%s')" % \
+                (result['userlist'][i]['userid'], result['userlist'][i]['name'])
+                #print sql #debug only
+                try:
+                    cursor.execute(write_db)
+                    db.commit()
+                except MySQLdb.Warning, w:  
+                    sqlWarning =  "Warning:%s" % str(w)
+                    print sqlWarning
+                except MySQLdb.Error, e:  
+                    sqlError =  "Error:%s" % str(e)
+                    print sqlError
+            else:
+                continue
+
+#获取用户详情           
+def store_user_detail(user_id): #钉钉有一些userid为0开始的，所以userid视为字符串
+    is_success, result = user.get_user(dingapi_timer.access_token, user_id)
+    global result
+    if is_success == True and result != None:
+##        write_db = "INSERT INTO dingding_user_detail(userid, orderInDepts, dingId, mobile, tel, \
+##                    workPlace, remark, isAdmin, isBoss, isHide, isLeaderInDepts, name, active, department, \
+##                    uposition, email, avatar, jobnumber) VALUES( \
+##                    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%s', '%s', \
+##                    '%d', '%s', '%s', '%s', '%s', '%s')" % \
+##                    (result['userid'], result['orderInDepts'], result['dingId'], result['mobile'], result['tel']
+##                     , result['workPlace'], result['remark'], result['isAdmin'], result['isBoss'], result['isHide']
+##                     , result['isLeaderInDepts'], result['name'], result['active'], result['department'], result['position']
+##                     , result['email'], result['avatar'], result['jobnumber'])
+        write_db = "INSERT INTO dingding_user_detail(userid, orderInDepts, dingId, mobile, tel, \
+                    workPlace, remark, isAdmin, isBoss, isHide, isLeaderInDepts, name, active, department, \
+                    uposition, email, avatar) VALUES( \
+                    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%s', '%s', \
+                    '%d', '%s', '%s', '%s', '%s')" % \
+                    (result['userid'], result['orderInDepts'], result['dingId'], result['mobile'], result['tel']
+                     , result['workPlace'], result['remark'], result['isAdmin'], result['isBoss'], result['isHide']
+                     , result['isLeaderInDepts'], result['name'], result['active'], result['department'], result['position']
+                     , result['email'], result['avatar'])        
+        #print write_db
         try:
-            cursor.execute(ding_read_sql)
+            cursor.execute(write_db)
             db.commit()
         except MySQLdb.Warning, w:  
             sqlWarning =  "Warning:%s" % str(w)
@@ -80,6 +159,37 @@ def read_department_detail_first(departmentid):
             sqlError =  "Error:%s" % str(e)
             print sqlError
 
+
+def store_all_user_detail():
+    global db_result
+    cursor.execute("SELECT id FROM dingding_department_detail")
+    db_result = cursor.fetchall() #获取含根部门的所有部门id
+    if db_result != None and len(db_result) > 0:
+        for i in range(len(db_result)):
+            store_user_list(db_result[i][0])
+
+    cursor.execute("SELECT userid FROM dingding_user_list")
+    db_result = cursor.fetchall() #获取含根部门的所有部门id
+    if db_result != None and len(db_result) > 0:
+        for i in range(len(db_result)):
+            store_user_detail(db_result[i][0])
     
+
+
+
+def user_one_key_store():
+    department_one_key_store()
+    store_all_user_detail()
+
+                    
+#debug only        
+def print_dict(result):
+    for k, v in result.iteritems():
+        print '%s : %s' % (k, v)       
     
+#store_user_list('56590897')
+#store_user_detail('135665061528025280')
+#store_all_user_detail()
+#user_one_key_store()
+#断开数据库
 db.close()
