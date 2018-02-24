f18d7fe83d0dd68f291d585839ab6d1f5071366d
fly-high2016 <fly-high@live.cn>
2018-02-24 12:24:25 +0800

    2018年2月24日12:22:19修改了以避免response未定义的错误
    如果API发生超时，返回词典-1和错误信息

--- ding_api/dingapi_timer.py
+++ ding_api/dingapi_timer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
+import time
 import threading
 import auth
 import config
--- ding_api/utils.py
+++ ding_api/utils.py
@@ -9,9 +9,9 @@ from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 
 path_log_file = '/tmp/dingding_test.log'
-#Windows下会向python解释器所在分区写入日志 | tmp文件夹需要预先手工建立
+#Windows下会向出错程序所在分区写入日志 | tmp文件夹需要预先手工建立
 
-logger = logging.getLogger('dingding')
+logger = logging.getLogger('DingAPI')
 file_handler = logging.FileHandler(path_log_file)
 formatter = logging.Formatter('%(asctime)s:%(name)s-->%(levelname)s %(message)s')
 file_handler.setFormatter(formatter)
@@ -19,15 +19,17 @@ logger.addHandler(file_handler)
 
 
 def http_get(url):
+    result = -1
     try:
         response = urllib2.urlopen(url, timeout=30)
+        result = json.loads(response.read())
     except urllib2.URLError, e:
-        logger.error(e)
-    result = json.loads(response.read())
+        logger.error(e)    
     return handle_result(result)
 
 
 def http_post(url, data):
+    result = -1
     headers = {
         "Content-Type": "application/json",
         "Accept-Charset": "utf-8"
@@ -35,13 +37,14 @@ def http_post(url, data):
     request = urllib2.Request(url, json.dumps(data), headers)
     try:
         response = urllib2.urlopen(request, timeout=30)
+        result = json.loads(response.read())
     except urllib2.URLError, e:
         logger.error(e)
-    result = json.loads(response.read())
     return handle_result(result)
 
 
 def http_upload(url, data):
+    result = -1
     # 在urllib2上注册http流处理句炳
     register_openers()
     # datagen 是一个生成器对象，返回编码过后的参数
@@ -50,25 +53,30 @@ def http_upload(url, data):
 
     try:
         response = urllib2.urlopen(request, timeout=30)
+        result = json.loads(response.read())
     except urllib2.URLError, e:
         logger.error(e)
-    result = json.loads(response.read())
-
     return handle_result(result)
 
 
 def http_download(url, media_file):
     try:
         response = urllib2.urlopen(url, timeout=30)
+        media_file.write(response.read())
+        media_file.close()
     except urllib2.URLError, e:
         logger.error(e)
-    media_file.write(response.read())
-    media_file.close()
+        
     return True, 'success'
 
 
 def handle_result(result):    
     #成功则返回True和结果，否则返回False和错误信息并写入日志
+    if result == -1:
+        errcode = -1
+        errmsg = 'http result is empty!'
+        logger.error("Error: %s | %s" % (errcode, errmsg))
+        return False, {errcode:errmsg}
     if result.get('errcode') == 0:
         result.pop('errcode')   #删除成功时的固定状态信息errcode=0
         result.pop('errmsg')    #删除成功时的固定状态信息errmsg=ok
