5ff2a891ae5e690c572cf1a5e3b2be8325e432e7
fly-high2016 <fly-high@live.cn>
2018-02-26 16:56:56 +0800

    2018年2月26日16:56:15更新快速读取钉钉和映射关系建立模块

--- ding_read_quick.py
+++ ding_read_quick.py
@@ -6,7 +6,10 @@ from dingding_sdk import user
 from dingding_sdk import department
 from dingding_sdk import utils
 import MySQLdb
-
+import os
+import threading
+import math
+from multiprocessing import Process, Pool, Queue
 
 
 def connect_db(address, user, password, database):
@@ -28,13 +31,8 @@ def close_db(db):
 
 
 #初始化部门清单
-def store_department_list(db, cursor, fetch_child=True, parent_id=1):
-    #钉钉获取部门列表的API不会返回根部门，所以手动指定根部门，根部门不能通过API进行修改，所以名称没有意义            
-    ding_root_sql = "REPLACE INTO dingding_department_list(`id`, `name`, `parentid`, `createDeptGroup`, `autoAddUser`) \
-                        VALUES('1', '中国联合网络通信集团有限公司', '0', '1', '1')"
-    cursor.execute(ding_root_sql)
-    db.commit()
-    #获取根部门以下所有部门清单
+def store_department_list(fetch_child=True, parent_id=1):
+    db, cursor = connect_db('localhost', 'root', 'yoyoball', 'dingtalk')
     #global result
     is_success, result = department.get_department_list(dingapi_timer.access_token, fetch_child, parent_id)
     #print result #debug only
@@ -53,14 +51,42 @@ def store_department_list(db, cursor, fetch_child=True, parent_id=1):
             except MySQLdb.Error, e:  
                 sqlError =  "Error:%s" % str(e)
                 print sqlError
+    db.close()
+    return True
 
 
 
+def store_department_detail(department_id):
+    db, cursor = connect_db('localhost', 'root', 'yoyoball', 'dingtalk')
+    is_success, result = department.get_department_detail(dingapi_timer.access_token, department_id)
+    #print result #debug only
+    if is_success == True and department_id != 1:  #id为1的为根部门，根部门钉钉不会返回parentid,在数据库中将根部门的parentid设置为0
+        ding_read_sql = "REPLACE INTO dingding_department_list(`id`, `name`, `parentid`, `createDeptGroup`, `autoAddUser`) \
+                        VALUES('%s', '%s', '%s', '%d', '%d')" % \
+                        (result['id'], result['name'], result['parentid'], result['createDeptGroup'], result['autoAddUser'])
+    else:
+        ding_read_sql = "REPLACE INTO dingding_department_list(`id`, `name`, `parentid`, `createDeptGroup`, `autoAddUser`) \
+                        VALUES('%s', '%s', '%s', '%d', '%d')" % \
+                        (result['id'], result['name'], '0', result['createDeptGroup'], result['autoAddUser'])    
+
+    #print ding_read_sql #debug only
+    try:
+        cursor.execute(ding_read_sql)
+        db.commit()
+    except MySQLdb.Warning, w:  
+        sqlWarning =  "Warning:%s" % str(w)
+        print sqlWarning
+    except MySQLdb.Error, e:  
+        sqlError =  "Error:%s" % str(e)
+        print sqlError
+    db.close()
+    return True
 
 
 #根据部门获取人员基本信息，对于具有相同userid的人员记录只存储一次
 #确保user_list表没有重复的人员
-def store_user_detail(department_id, db, cursor, offset=None, size=None, order=None):
+def store_user_detail(department_id, offset=None, size=None, order=None):
+    db, cursor = connect_db('localhost', 'root', 'yoyoball', 'dingtalk')
     is_success, result = user.get_department_detail_userlist(dingapi_timer.access_token, department_id, offset, size, order)
     if is_success == True and result != None:
         for i in range(len(result['userlist'])):            
@@ -76,34 +102,95 @@ def store_user_detail(department_id, db, cursor, offset=None, size=None, order=N
             except MySQLdb.Error, e:  
                 sqlError =  "Error:%s" % str(e)
                 print sqlError
+    db.close()
+    return True
 
 
 
-
-
-def store_all_user_detail(db, cursor):
-    #global db_result #debug only
-    cursor.execute("SELECT id FROM dingding_department_list")
-    db_result = cursor.fetchall() #获取不含根部门的所有部门id
-    if db_result != None and len(db_result) > 0:
-        for i in range(len(db_result)):
-            store_user_detail(db_result[i][0], db, cursor)
-            
-    store_user_detail('1', db, cursor)
-
-    
-
-
-
-def ding_one_key_store():
+def ding_one_key_store():             
     #获取access_token可以全局使用dingapi_timer.access_token
     dingapi_timer.access_token_timer()
     #连接数据库
-    ding_db, ding_cursor = connect_db('localhost', 'root', 'yoyoball', 'dingtalk') 
-    #获取所有部门基本信息存储在dingding_department_list
-    store_department_list(ding_db, ding_cursor, fetch_child=True, parent_id=1)
-    #获取所有人员信息人员详情并存储在dingding_user_detail
-    store_all_user_detail(ding_db, ding_cursor)
+    ding_db, ding_cursor = connect_db('localhost', 'root', 'yoyoball', 'dingtalk')
+    #钉钉获取部门列表的API不会返回根部门，所以手动指定根部门，根部门不能通过API进行修改，所以名称没有意义            
+    ding_root_sql = "REPLACE INTO dingding_department_list(`id`, `name`, `parentid`, `createDeptGroup`, `autoAddUser`) \
+                        VALUES('1', '中国联合网络通信集团有限公司', '0', '1', '1')"
+    ding_cursor.execute(ding_root_sql)
+    ding_db.commit()    
+    
+    #获取根部门的所有直接下级id
+    for i in range(5):
+        status, class1_result = department.get_sub_dept_id_list(dingapi_timer.access_token, 1)
+        if status == True:
+            break
+        if i == 4 and status == False:
+            #print 'error' #debug only 
+            return -1
+    #print class1_result['sub_dept_id_list'] #debug only
+
+        
+##    #使用子进程获取根部门直接下级的组织清单
+##    for i in range(len(class1_result['sub_dept_id_list'])):
+##        store_department_detail(class1_result['sub_dept_id_list'][i])
+##    department_pool = Pool()
+##    for i in range(len(class1_result['sub_dept_id_list'])):
+##        department_pool.apply_async(store_department_list, args=(True, class1_result['sub_dept_id_list'][i]))
+##    department_pool.close()
+##    department_pool.join()
+##    #使用子进程获取所有人员详情
+##    user_pool = Pool()
+##    ding_dept_num_sql = "SELECT count(*) FROM dingtalk.dingding_department_list;"
+##    ding_cursor.execute(ding_dept_num_sql)
+##    ding_dept_num = ding_cursor.fetchone()
+##    print ding_dept_num #debug only
+##    if ding_dept_num == None:
+##        return -2
+##    group_factor = 10.0 #设定分组单位，需要小数位为零
+##    group_number = int(math.ceil(ding_dept_num[0] / group_factor)) 
+##    for i in range(group_number):        
+##        ding_dept_group_sql = "SELECT `id` FROM dingtalk.dingding_department_list limit %s,%s;" % (int(i*group_factor), int((i+1)*group_factor-1))
+##        print ding_dept_group_sql #debug only
+##        ding_cursor.execute(ding_dept_group_sql)
+##        dept_group = ding_cursor.fetchall()
+##        for j in range(len(dept_group)):
+##            user_pool.apply_async(store_user_detail, args=(class1_result['sub_dept_id_list'][j]))
+##    user_pool.close()
+##    user_pool.join()
+
+
+
+
+    #多线程获取部门清单
+    for i in range(len(class1_result['sub_dept_id_list'])):
+        store_department_detail(class1_result['sub_dept_id_list'][i])
+    for i in range(len(class1_result['sub_dept_id_list'])):
+        #thread.start_new_thread(store_department_list, (True, class1_result['sub_dept_id_list'][i]))
+        dept_thread = threading.Thread(target=store_department_list, args=(True, class1_result['sub_dept_id_list'][i]))
+        dept_thread.start()
+        dept_thread.join()
+
+       
+    #多线程获取人员详情
+    ding_dept_num_sql = "SELECT count(*) FROM dingtalk.dingding_department_list;"
+    ding_cursor.execute(ding_dept_num_sql)
+    ding_dept_num = ding_cursor.fetchone()
+    print ding_dept_num #debug only
+    if ding_dept_num == None:
+        return -2
+    group_factor = 10.0#设定分组单位，需要小数位为零
+    group_number = int(math.ceil(ding_dept_num[0] / group_factor)) 
+    for i in range(group_number):        
+        ding_dept_group_sql = "SELECT `id` FROM dingtalk.dingding_department_list limit %s,%s;" % (int(i*group_factor), int((i+1)*group_factor-1))
+        print ding_dept_group_sql #debug only
+        ding_cursor.execute(ding_dept_group_sql)
+        dept_group = ding_cursor.fetchall()
+        for j in range(len(dept_group)):
+            #thread.start_new_thread(store_user_detail, (dept_group[j][0],))
+            user_thread = threading.Thread(target=store_user_detail, args=(dept_group[j][0],))
+            user_thread.start()
+            user_thread.join()
+
+
     #断开数据库
     close_db(ding_db)
     return True
